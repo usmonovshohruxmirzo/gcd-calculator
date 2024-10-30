e6e195eafb09e5775512d2d9aeed1b64ea0e9dcb
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-10-30 22:25:26 +0500

    feat: setup.py updated

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='gcd_calculator',
     version='0.1.2',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[],
     author='Usmonov Shoxruxmirzo',
     author_email='usmonovshohruxmirzo@gmail.com',
