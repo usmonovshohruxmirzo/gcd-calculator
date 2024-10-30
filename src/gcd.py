9512b94b07ae6bdfbc4b83c33c0c0e1d9ca6d6ab
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-10-30 21:41:13 +0500

    feat: gcd.py updated

--- src/gcd.py
+++ src/gcd.py
@@ -4,10 +4,3 @@ def gdc(a: int, b: int):
     while b:
         a, b = b, a % b
     return a
-
-def get_api_key_from_config():
-    with open('config.json') as f:
-        config = json.load(f)
-        return config['api_key']
-
-api_key = get_api_key_from_env()
